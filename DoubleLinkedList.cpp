d09873dcc78a52ddac517d77540e43df923b3f83
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-05-15 12:19:21 +0700

    Traverse

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -82,6 +82,78 @@ class Doublelinkedlist
             current->next = newNode; //step 9d
         }
 
+        void hapus()
+        {
+            if (START == NULL)
+            {
+                cout << "\nlist is empty" << endl;
+                return;
+            }
+
+            cout << "\nEnter the roll number of the student whose record is to be delete:";
+            int rollNo;
+            cin >> rollNo;
+
+            Node *current = START;
+
+            //step 1 : traverse the list to find the node
+            while (current != NULL && current->nomMhs != rollNo)
+                current = current->next;
+
+            if (current == NULL)
+            {
+                cout << "record not found" << endl;
+                return;
+            }
+
+            //step 2: if mode is at the beginning
+            if (current == START)
+            {
+                START = current->next; //step 2a
+                if (START != NULL)
+                    START->prev = NULL; // STEP 2b
+
+            }
+            else
+            {
+                // step 3: link previous node to next of current
+                current->prev->next = current->next;
+
+                // step4 : if current is not the last node
+                if (current->next !=NULL)
+                    current->next->prev = current->prev;
+            }
+
+            //step 5: delete the node
+            delete current;
+            cout << "record with roll number" << rollNo << "delete" << endl;
+
+        }
+
+        void traverse()
+        {
+            if (START == NULL)
+            {
+                cout << "\nlist in empty" << endl;
+                return;
+            }
+            // step1 : mark first node as currentNode
+            Node *currentNode = START;
+
+            // step2 : repeat untill currentNode == NULL
+            cout << "\nrecord in ascending order of roll number are:\n";
+            int i = 0;
+            while (currentNode != NULL)
+            {
+                cout << i + 1 << "."<< currentNode->nomMhs << " " <<endl;
+
+            // step 3 : move to next mode
+            currentNode = currentNode->next;
+            i++;
+            }
+
+        }
+
         
 };
 
